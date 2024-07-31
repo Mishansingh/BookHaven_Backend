8af52080c61af7f0acbe0dad1e3455f8b2b8a2ce
Mishan Singh <121171191+Mishansingh@users.noreply.github.com>
2024-08-01 03:23:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-const URI = process.env.MongoDBURI;
+const URI = "mongodb+srv://mishansingh111:Mishan@123@cluster0.1y7wccr.mongodb.net/";
 
 // connect to mongoDB
 try {
